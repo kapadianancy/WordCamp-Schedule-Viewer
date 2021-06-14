dfac99de6508a00e32527b69fa1ca79315d483a1
doctz <kapadianancy21@gmail.com>
2021-06-14 22:24:47 +0530

    feature/header #refine webpack,header design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,59 @@
-import React from 'react'
-import NancyProfile from '../../../public/images/nancy-profile.jpeg'
-function Header (props) {
-  const imageStyle = {
-    height: '50px',
-    width: '50px'
-  }
-  return (
+import React from 'react';
+import NancyProfile from '../../../public/assets/images/nancy-profile.jpeg';
+
+function Header(props) {
+    var divStyle = {
+        color: 'red'
+    }
+    var style={
+        height:70
+    }
+    return (
         <>
-           <img src={ NancyProfile} style={imageStyle} />
+            <div className="navbar navbar-expand-md navbar-dark" style={style}>
+                <div className="navbar-brand">
+                    <a href="#" className="d-inline-block">
+                        {/* <img src="../assets/images/logo_light.png" alt=""/> */}
+                        <h1 style={{color: "white", fontWeight: "bold", fontStyle: "italic"}}>WordCamp Scheduler</h1>
+                    </a>
+                </div>
+
+
+                <div className="collapse navbar-collapse" id="navbar-mobile">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a href="#"
+                               className="navbar-nav-link sidebar-control sidebar-main-toggle d-none d-md-block">
+                                <i className="icon-paragraph-justify3"></i>
+                            </a>
+                        </li>
+
+
+                    </ul>
+
+                    <span className="badge bg-success ml-md-3 mr-md-auto">Online</span>
+
+                    <ul className="navbar-nav">
+
+
+                        <li className="nav-item dropdown dropdown-user">
+                            <a href="#" className="navbar-nav-link d-flex align-items-center dropdown-toggle"
+                               data-toggle="dropdown">
+                                 <img src={NancyProfile} className="rounded-circle mr-2" height="34" alt=""/>
+                                {/*<Avatar name={user == null ? null : user.name} round={true} size="50" color="#26a69a"*/}
+                                {/*        style={{margin: "5px 10px 10px auto"}}/>*/}
+                                <span>Nancy Kapadia</span>
+                            </a>
+
+                            <div className="dropdown-menu dropdown-menu-right">
+
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </>
-  )
+    );
 }
-export default Header
+
+export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,89 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack');
+const {merge} = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var NpmInstallPlugin = require('npm-install-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+const path=require('path');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/index'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      include: path.resolve(__dirname, 'src'),
-      use: ['babel-loader']
+const TARGET = process.env.npm_lifecycle_event;
+console.log("target event is " + TARGET);
+
+var common = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        chunkFilename: '[id].js',
+        publicPath: ''
     },
-    {
-      test: /\.(png|jpg|jpeg|gif)$/,
-      include: path.resolve(__dirname, 'public'),
-      loader: 'file-loader'
-    }
-    ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    port: 9000
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html' // source html
-    })
-  ]
+    resolve: {
+        extensions: ['.js', '.jsx','.css']
+    },
+    module: {
+        rules: [{
+            test: /\.js[x]?$/,
+            use: ['babel-loader'],
+            exclude: /(node_modules)/
+        }, {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+        }, {
+            test: /\.less$/,
+            use: ['style', 'css', 'less']
+        }, {
+            test: /\.woff$/,
+            use: ["url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"]
+        }, {
+            test: /\.woff2$/,
+            use: ["url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"]
+        }, {
+            test: /\.(eot|ttf|svg|gif|png|jpeg)$/,
+            use: ["file-loader"]
+        }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+};
+
+if (TARGET === 'dev' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            historyApiFallback: true
+        },
+        output: {
+            publicPath: 'http://localhost:8080/public'
+        },
+        plugins: [
+            new NpmInstallPlugin({
+                save: true // --save
+            })
+        ]
+    });
+}
+
+if (TARGET === 'build' || TARGET=="start") {
+    module.exports = merge(common, {
+        devtool: 'source-map',
+        output: {
+            path: path.resolve(__dirname, 'dist')
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Webpack-react',
+                template: './public/index.html'
+            })
+        ]
+    });
 }
