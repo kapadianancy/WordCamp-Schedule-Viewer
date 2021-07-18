d5bf241705bba9f2350f67863b4d68015dcbb2cf
kapadianancy <kapadianancy21@gmail.com>
2021-07-18 18:54:53 +0530

    development #changes in index

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './index.js',
   output:
     {
-      path: path.resolve(__dirname, 'public'),
+      path: path.resolve(__dirname, 'dist'),
       filename: 'index_bundle.js'
 
     },
