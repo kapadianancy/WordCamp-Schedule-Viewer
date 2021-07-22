8aad53033d3f8ff0819583f9155572dc8f29bf7e
kapadianancy <kapadianancy21@gmail.com>
2021-07-22 20:50:32 +0530

    final changes/clenup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import '../public/assets/js/app'
 
 const rootElement = document.getElementById('root')
 
